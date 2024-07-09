997f75b625011ed6dc2ff31ad79bd96111e6cac1
Farhan Aulianda <70313408+lianstemp@users.noreply.github.com>
2024-07-09 22:13:11 +0700

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const PORT = 80;
+const TITLE = process.env.TITLE || 'Default Title';
+const MESSAGE = process.env.MESSAGE || 'Default Message';
+
+const requestHandler = (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`Title: ${TITLE}\nMessage: ${MESSAGE}`);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
